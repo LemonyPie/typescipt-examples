d99d662a1799d4ff1536d0cf4133770fe8961587
Artem Senin <artem.senin@nordigy.ru>
2020-05-07 23:18:27 +0300

    feat: add recursive binary search variation

--- src/algorythms/recursive-binary-search.ts
+++ src/algorythms/recursive-binary-search.ts
@@ -1,24 +1,28 @@
-const search = (list: number[] | string [], item: number | string): number | string | null => {
-  const low = 0;
-  const high = list.length - 1;
-  const mid = Math.floor((low + high) / 2);
-  const guess = list[mid];
+const binary_search = <T extends (number | string)>( list: T[], item: T): number | null => {
+  const search = (list: T[], item: T, bias: number ): number | null => {
+    const low = 0;
+    const high = list.length - 1;
+    const mid = Math.floor( ( low + high ) / 2 );
+    const guess = list[mid];
 
-  if (low > high) {
-    return null;
+    if ( low > high ) {
+      return null;
+    }
+
+    if ( guess === item ) {
+      return bias + mid;
+    } else if ( guess > item ) {
+      return search( list.slice( 0, mid - 1 ), item, bias)
+    } else {
+      return search( list.slice( mid + 1, list.length ), item, bias + mid + 1);
+    }
   }
 
-  if( guess === item) {
-    return mid;
-  } else if (guess > item) {
-    return search( list.slice( 0, mid - 1 ), item )
-  } else {
-    return search( list.slice( mid + 1, list.length ), item );
-  }
-
+  return search(list, item, 0);
 };
 
-// search([1], 1); //?
-// search([1], 10); //?
-// search([1,10,12], 10); //?
-search([1,2,3,4,5,6,10,12], 10); //?
+// binary_search([1], 1); //?
+// binary_search([1], 10); //?
+// binary_search([1,10,12], 10); //?
+// binary_search([1,2,3,4,5,6,10,12], 10); //?
+binary_search([1,2,3,4,5,6,10,12], 2); //?
